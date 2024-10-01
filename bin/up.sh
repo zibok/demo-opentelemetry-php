119497960532e500d1ed8038f13e68058a29353d
Vincent Lepot <vlepot@deezer.com>
2024-10-01 19:34:35 +0200

    Avoid package-lock.json updates at install time

--- bin/up.sh
+++ bin/up.sh
@@ -9,5 +9,5 @@ if [ "$1" == "--rebuild-apps" ]; then
     docker compose exec dev bash -c "composer install -d /demo-opentelemetry-php/$app"
   done
 
-  docker compose exec dev bash -c "cd /demo-opentelemetry-php/lesfilmsquejekiffe && npm install && npm run build"
+  docker compose exec dev bash -c "cd /demo-opentelemetry-php/lesfilmsquejekiffe && npm install --no-save && npm run build"
 fi
