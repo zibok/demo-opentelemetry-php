4395ea565dd3dc50db3cdbbd3e34cb97a1f84d3b
Vincent Lepot <vlepot@deezer.com>
2024-10-09 22:32:25 +0200

    Fix composer cache

--- bin/install-all-otel-composer-deps.sh
+++ bin/install-all-otel-composer-deps.sh
@@ -3,7 +3,7 @@
 for dir in lesfilmsquejekiffe favlist-svc catalog-svc user-svc ; do
     (
         cd $dir
-        composer require \
+        composer require --no-interaction \
             "open-telemetry/opentelemetry-auto-symfony ^1.0@beta" \
             open-telemetry/opentelemetry-auto-pdo \
             open-telemetry/sdk \
